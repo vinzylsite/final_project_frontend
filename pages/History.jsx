d1f5e92f64f9e75ff77ae622e687d618fc1ffd1c
vinzylsite <allyssavinoya23@gmail.com>
2026-03-11 09:36:36 +0800

    Delete pages directory

--- pages/History.jsx
+++
@@ -1,8 +0,0 @@
-import { Typography } from '@mui/material';
-
-export default function History() {
-  return (
-    <Typography variant="h5">Your watch history will appear here.</Typography>
-  );
-}
- 
--- pages/Subscriptions.jsx
+++
@@ -1,8 +0,0 @@
-import { Typography } from '@mui/material';
-
-export default function Subscriptions() {
-  return (
-    <Typography variant="h5">Your subscriptions will appear here.</Typography>
-  );
-}
- 
